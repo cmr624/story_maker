847455ec6da591bf0a24ed334441d21a6c143d99
Carlos Michael Rodriguez <cmr624@nyu.edu>
2021-03-22 22:51:04 -0400

    added working firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-const config = {
-  apiKey: "AIzaSyAsdY2YHQDAVJ9ZJgsqNVB2kaB3A5jVmNY",
-  authDomain: "fibbage-b1e4d.firebaseapp.com",
-  databaseURL: "https://fibbage-b1e4d.firebaseio.com",
-  projectId: "fibbage-b1e4d",
-  storageBucket: "fibbage-b1e4d.appspot.com",
+// const config = {
+//   apiKey: "AIzaSyAsdY2YHQDAVJ9ZJgsqNVB2kaB3A5jVmNY",
+//   authDomain: "fibbage-b1e4d.firebaseapp.com",
+//   databaseURL: "https://fibbage-b1e4d.firebaseio.com",
+//   projectId: "fibbage-b1e4d",
+//   storageBucket: "fibbage-b1e4d.appspot.com",
+// };
+
+const config = { 
+    apiKey: "AIzaSyCcr5ogB7GI52x6ihjri-geoIw_YcVQpGA",
+    authDomain: "story-maker-c573f.firebaseapp.com",
+    databaseURL:"https://story-maker-c573f-default-rtdb.firebaseio.com/",
+    // databaseURL:"https://story-maker-c573f.firebaseio.com",
+    projectId: "story-maker-c573f",
+    storageBucket: "story-maker-c573f.appspot.com",
 };
+
 
 class Firebase {
     constructor() {
